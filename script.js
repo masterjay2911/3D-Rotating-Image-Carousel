8220b046c9e672959fbd286446bc98b2e2379119
Master Jay <169078298+masterjay2911@users.noreply.github.com>
2025-12-17 13:07:20 +0530

    script.js

--- script.js
+++ script.js
@@ -7,101 +7,115 @@ var imgHeight = 170;
 var bgMusicURL = 'https://user-images.githubusercontent.com/151072490/283747943-7b08424b-8647-4bdc-996c-965063dbb5e3.mp4';
 var bgMusicControls = true; 
 
-setTimeout(init, 1000);
+// Wait until DOM is fully loaded
+window.addEventListener('DOMContentLoaded', () => {
 
-var odrag = document.getElementById('drag-container');
-var ospin = document.getElementById('spin-container');
-var aImg = ospin.getElementsByTagName('img');
-var aVid = ospin.getElementsByTagName('video');
-var aEle = [...aImg, ...aVid];
+    var odrag = document.getElementById('drag-container');
+    var ospin = document.getElementById('spin-container');
+    var aImg = ospin.getElementsByTagName('img');
+    var aVid = ospin.getElementsByTagName('video');
+    var aEle = [...aImg, ...aVid];
 
+    ospin.style.width = imgWidth + "px";
+    ospin.style.height = imgHeight + "px";
 
-ospin.style.width = imgWidth + "px";
-ospin.style.height = imgHeight + "px";
+    var ground = document.getElementById('ground');
+    ground.style.width = radius * 3 + "px";
+    ground.style.height = radius * 3 + "px";
 
-var ground = document.getElementById('ground');
-ground.style.width = radius * 3 + "px";
-ground.style.height = radius * 3 + "px";
+    function init(delayTime) {
+      for (var i = 0; i < aEle.length; i++) {
+        aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
+        aEle[i].style.transition = "transform 1s";
+        aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
+      }
+    }
 
-function init(delayTime) {
-  for (var i = 0; i < aEle.length; i++) {
-    aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
-    aEle[i].style.transition = "transform 1s";
-    aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
-  }
-}
+    function applyTranform(obj) {
+      if(tY > 180) tY = 180;
+      if(tY < 0) tY = 0;
+      obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+    }
 
-function applyTranform(obj) {
- 
-  if(tY > 180) tY = 180;
-  if(tY < 0) tY = 0;
+    function playSpin(yes) {
+      ospin.style.animationPlayState = (yes?'running':'paused');
+    }
 
-  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
-}
+    var sX, sY, nX, nY, desX = 0,
+        desY = 0,
+        tX = 0,
+        tY = 10;
 
-function playSpin(yes) {
-  ospin.style.animationPlayState = (yes?'running':'paused');
-}
+    if (autoRotate) {
+      var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
+      ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
+    }
 
-var sX, sY, nX, nY, desX = 0,
-    desY = 0,
-    tX = 0,
-    tY = 10;
-
-if (autoRotate) {
-  var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
-  ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
-}
-
-if (bgMusicURL) {
-  document.getElementById('music-container').innerHTML += `
+    if (bgMusicURL) {
+      document.getElementById('music-container').innerHTML += `
 <audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
 <p>If you are reading this, it is because your browser does not support the audio element.</p>
 </audio>
 `;
-}
+    }
 
-document.onpointerdown = function (e) {
-  clearInterval(odrag.timer);
-  e = e || window.event;
-  var sX = e.clientX,
-      sY = e.clientY;
-
-  this.onpointermove = function (e) {
-    e = e || window.event;
-    var nX = e.clientX,
-        nY = e.clientY;
-    desX = nX - sX;
-    desY = nY - sY;
-    tX += desX * 0.1;
-    tY += desY * 0.1;
-    applyTranform(odrag);
-    sX = nX;
-    sY = nY;
-  };
-
-  this.onpointerup = function (e) {
-    odrag.timer = setInterval(function () {
-      desX *= 0.95;
-      desY *= 0.95;
-      tX += desX * 0.1;
-      tY += desY * 0.1;
-      applyTranform(odrag);
-      playSpin(false);
-      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+    // Add pointer + touch support
+    const pointerDown = (e) => {
         clearInterval(odrag.timer);
-        playSpin(true);
-      }
-    }, 17);
-    this.onpointermove = this.onpointerup = null;
-  };
+        e = e || window.event;
+        sX = e.clientX || e.touches[0].clientX;
+        sY = e.clientY || e.touches[0].clientY;
 
-  return false;
-};
+        const pointerMove = (e) => {
+            nX = e.clientX || e.touches[0].clientX;
+            nY = e.clientY || e.touches[0].clientY;
+            desX = nX - sX;
+            desY = nY - sY;
+            tX += desX * 0.1;
+            tY += desY * 0.1;
+            applyTranform(odrag);
+            sX = nX;
+            sY = nY;
+        };
 
-document.onmousewheel = function(e) {
-  e = e || window.event;
-  var d = e.wheelDelta / 20 || -e.detail;
-  radius += d;
-  init(1);
-};
+        const pointerUp = () => {
+            odrag.timer = setInterval(function () {
+                desX *= 0.95;
+                desY *= 0.95;
+                tX += desX * 0.1;
+                tY += desY * 0.1;
+                applyTranform(odrag);
+                playSpin(false);
+                if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+                    clearInterval(odrag.timer);
+                    playSpin(true);
+                }
+            }, 17);
+
+            window.removeEventListener('pointermove', pointerMove);
+            window.removeEventListener('pointerup', pointerUp);
+            window.removeEventListener('touchmove', pointerMove);
+            window.removeEventListener('touchend', pointerUp);
+        };
+
+        window.addEventListener('pointermove', pointerMove);
+        window.addEventListener('pointerup', pointerUp);
+        window.addEventListener('touchmove', pointerMove);
+        window.addEventListener('touchend', pointerUp);
+
+        return false;
+    };
+
+    odrag.addEventListener('pointerdown', pointerDown);
+    odrag.addEventListener('touchstart', pointerDown);
+
+    document.onmousewheel = function(e) {
+      e = e || window.event;
+      var d = e.wheelDelta / 20 || -e.detail;
+      radius += d;
+      init(1);
+    };
+
+    init();
+
+});
